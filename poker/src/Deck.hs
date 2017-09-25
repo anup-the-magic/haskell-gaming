8fd16e2f90c21b1a47ff99de76514c80e94777c0
Anup Kishore <anup@secondspectrum.com>
2017-09-25 03:37:27 -0700

    Initial (WIP) work on haskell poker project

---
+++ poker/src/Deck.hs
@@ -0,0 +1,43 @@
+module Deck where
+
+data Suit
+  = Club
+  | Diamond
+  | Heart
+  | Spade
+  deriving (Read, Show, Enum, Eq, Ord)
+
+data Rank
+  = Two
+  | Three
+  | Four
+  | Five
+  | Six
+  | Seven
+  | Eight
+  | Nine
+  | Ten
+  | Jack
+  | Queen
+  | King
+  | Ace
+  deriving (Read, Show, Enum, Eq, Ord)
+
+data Card = Card
+  { suit :: Suit
+  , rank :: Rank
+  } deriving (Read, Show, Eq)
+
+instance Ord Card where
+  compare c1 c2 = compare (rank c1, suit c1) (rank c2, suit c2)
+
+instance Enum Card where
+  fromEnum c = 4 * (fromEnum . rank) c + (fromEnum . suit) c
+  toEnum n =
+    let (rank', suit') = divMod n 4
+    in Card {suit = toEnum suit', rank = toEnum rank'}
+
+type Deck = [Card]
+
+empty :: Deck
+empty= [Card {suit = s, rank = r} | s <- [Club ..  Diamond], r <- [Two .. Ace]]
---
+++ poker/src/Poker.hs
@@ -0,0 +1,103 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+
+module Poker where
+
+import qualified Deck
+import Deck (Card, Deck)
+
+data Player = Player
+  { _id :: Int
+  , hand :: [Card]
+  , stack :: Int
+  }
+
+type Pot = Int
+
+data Board
+  = PreFlop Pot
+  | Flop Pot
+         Card
+         Card
+         Card
+  | Turn Pot
+         Card
+         Card
+         Card
+         Card
+  | River Pot
+          Card
+          Card
+          Card
+          Card
+          Card
+
+data Game = Game
+  { _id :: Int
+  , deck :: Deck
+  , players :: [Player]
+  , board :: Maybe Board
+  }
+
+getId :: Int
+getId = 1
+
+initialize :: Int -> Int -> Game
+initialize n buyIn =
+  Game
+  { _id = getId
+  , deck = Deck.empty
+  , players = map (\x -> Player {_id = x, hand = [], stack = buyIn}) [0 .. n]
+  , board = Nothing
+  }
+
+-- TODO
+decideWinner :: Game -> Game
+decideWinner = id
+
+advanceState' :: Game -> (Pot, [Player]) -> Game
+advanceState' game@Game {board = Nothing} (pot, players) =
+  game {players = players, board = Just $ PreFlop pot}
+advanceState' game@Game {board = Just (PreFlop _), deck = c1:c2:c3:deck} (pot, players) =
+  game {players = players, board = Just $ Flop pot c1 c2 c3, deck = deck}
+advanceState' game@Game {board = Just (Flop _ c1 c2 c3), deck = c4:deck} (pot, players) =
+  game {players = players, board = Just $ Turn pot c1 c2 c3 c4, deck = deck}
+advanceState' game@Game {board = Just (Turn _ c1 c2 c3 c4), deck = c5:deck} (pot, players) =
+  game {players = players, board = Just $ River pot c1 c2 c3 c4 c5, deck = deck}
+advanceState' game@Game {board = Just (River _ c1 c2 c3 c4 c5)} (pot, players) =
+  game {board = Just $ River pot c1 c2 c3 c4 c5, players = players}
+
+-- TODO
+betting :: Pot -> [Player] -> [Card] -> (Pot, [Player])
+betting pot ps cs = (pot, ps)
+
+getPot :: Game -> Pot
+getPot Game {board = Nothing} = 0
+getPot Game {board = Just (PreFlop p)} = p
+getPot Game {board = Just (Flop p _ _ _)} = p
+getPot Game {board = Just (Turn p _ _ _ _)} = p
+getPot Game {board = Just (River p _ _ _ _ _)} = p
+
+getCards :: Game -> [Card]
+getCards Game {board = Nothing} = []
+getCards Game {board = Just (PreFlop _)} = []
+getCards Game {board = Just (Flop _ c1 c2 c3)} = [c1, c2, c3]
+getCards Game {board = Just (Turn _ c1 c2 c3 c4)} = [c1, c2, c3, c4]
+getCards Game {board = Just (River _ c1 c2 c3 c4 c5)} = [c1, c2, c3, c4, c5]
+
+advanceState :: Game -> Game
+advanceState game = advanceState' game $ betting (getPot game) (players game) (getCards game)
+
+-- TODO
+playHand :: Game -> Game
+playHand = decideWinner . advanceState . advanceState . advanceState
+
+removeLosers :: Game -> Game
+removeLosers game@Game {players = ps} = game {players = filter ((<= 0) . stack) ps}
+
+checkWinners :: Game -> Either (Maybe Player) Game
+checkWinners Game {players = [p]} = Left . Just $ p
+checkWinners Game {players = []} = Left Nothing
+checkWinners game = Right game
+
+play :: Game -> Maybe Player
+play = either id play . checkWinners . removeLosers . playHand
